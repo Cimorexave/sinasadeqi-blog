d7590fe6c2c84d738ee11421d46a9cd067819e14
Cimorexave <sadeq.s502@gmail.com>
2022-11-01 12:28:01 +0330

    feat: sidebar backdrop

--- src/components/home/Sidebar.tsx
+++ src/components/home/Sidebar.tsx
@@ -1,20 +1,33 @@
-import React from 'react'
-
-const Sidebar = () => {
+import React from "react";
+interface SidebarInterface {
+  sidebarIsOpen: boolean;
+  setSidebarIsOpen: Function;
+}
+const Sidebar = ({ sidebarIsOpen, setSidebarIsOpen }: SidebarInterface) => {
   return (
     <>
-    {/* desktop  */}
-    <aside className='sidebar sidebar-desktop hidden lg:flex fixed top-0 h-screen w-max'>
-      Sidebar 
-    </aside>
+      {/* desktop  */}
+      <aside className="sidebar sidebar-desktop hidden lg:flex fixed top-0 h-screen w-max">
+        Sidebar
+      </aside>
 
-    {/* mobile */}
-    <aside className='sidebar sidebar-mobile lg:hidden flex'>
-      Sidebar
-    </aside>
-    
+      {/* mobile */}
+      <aside
+        className={
+          "sidebar sidebar-mobile lg:hidden flex h-screen w-screen absolute top-0 right-0" +
+          sidebarIsOpen
+            ? "hidden"
+            : "flex"
+        }
+      >
+        <div
+          onClick={() => setSidebarIsOpen(false)}
+          className="backdrop basis-1/3 bg-black opacity-50"
+        ></div>
+        <div className="sidebar-content basis-2/3">Sidebar</div>
+      </aside>
     </>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -4,11 +4,14 @@ import Navbar from "../components/home/Navbar";
 import Sidebar from "../components/home/Sidebar";
 
 const Home = () => {
-  const [sidebarIsOpen, setSidebarIsOpen] = useState<boolean>(false)
+  const [sidebarIsOpen, setSidebarIsOpen] = useState<boolean>(false);
   return (
     <div className="home w-screen">
       <Navbar setSidebarIsOpen={setSidebarIsOpen} />
-      <Sidebar />
+      <Sidebar
+        sidebarIsOpen={sidebarIsOpen}
+        setSidebarIsOpen={setSidebarIsOpen}
+      />
     </div>
   );
 };
